7414ba12b0317b4e2593cbec11a69db0b9266c50
Naizoom <naizmansoori@gmail.com>
2023-06-20 18:12:56 +0530

    ajax-file

---
+++ ajaxData.php
@@ -0,0 +1,30 @@
+<?php
+include('../dbconn.php');
+
+if(isset($_POST["country_id"]) && !empty($_POST["country_id"])){
+    $query = $conn->query("SELECT * FROM states WHERE country_id = ".$_POST['country_id']." AND status = 1 ORDER BY state_name ASC");
+    $rowCount = $query->num_rows;
+    if($rowCount > 0){
+        echo '<option value="">Select state</option>';
+        while($row = $query->fetch_assoc()){ 
+            echo '<option value="'.$row['state_id'].'">'.$row['state_name'].'</option>';
+        }
+    }else{
+        echo '<option value="">State not available</option>';
+    }
+}
+
+if(isset($_POST["state_id"]) && !empty($_POST["state_id"])){
+    $query = $conn->query("SELECT * FROM cities WHERE state_id = ".$_POST['state_id']." AND status = 1 ORDER BY city_name ASC");
+    $rowCount = $query->num_rows;
+    if($rowCount > 0){
+        echo '<option value="">Select city</option>';
+        while($row = $query->fetch_assoc()){ 
+            echo '<option value="'.$row['city_name_slug'].'">'.$row['city_name'].'</option>';
+        }
+    }else{
+        echo '<option value="">City not available</option>';
+    }
+}
+
+?>
