6c15c270f5e7ed301127d06ef91e4584dec0a263
Sarful04 <78296018+Sarful04@users.noreply.github.com>
2023-06-20 18:14:28 +0530

    Update Attendance.dart

--- Attendance.dart
+++ Attendance.dart
@@ -5,7 +5,7 @@ import 'package:school_management/Screens/home.dart';
 import 'package:school_management/Widgets/AppBar.dart';
 import 'package:school_management/Widgets/MainDrawer.dart';
 import 'package:school_management/Widgets/UserDetailCard.dart';
-
+import 'package:school_management/Widgets/AppBar.dart';
 class Attendance extends StatefulWidget {
   @override
   _AttendanceState createState() => _AttendanceState();
